69429fbe068305253e0e17e37afd7c8200ba4406
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2022-12-23 20:36:18 +0100

    added query to extract orders to prepare periodically

--- order_management/src/main/java/it/unito/edu/scavolini/order_management/repository/OrderRepository.java
+++ order_management/src/main/java/it/unito/edu/scavolini/order_management/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package it.unito.edu.scavolini.order_management.repository;
 
 import it.unito.edu.scavolini.order_management.model.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,6 +10,15 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
 
     List<Order> findAll();
 
-    Order findDistinctFirstById(int id);
+    Order findDistinctFirstById(Long id);
+
+    //TODO fix SQL syntax
+    @Query(value = "SELECT * FROM order WHERE (" +
+        "order_type = 'DELIVERY' " +
+        "OR order_type = 'PREORDER' " +
+        "OR order_type = 'TAKEAWAY') " +
+        "AND order_state = 'ACCEPTED' " +
+        "AND date_time <= NOW() + INTERVAL 1 HOUR", nativeQuery = true)
+    List<Order> findOrdersToPrepare();
 
 }
