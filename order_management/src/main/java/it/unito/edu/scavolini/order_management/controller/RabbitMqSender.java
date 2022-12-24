ed1415b366a178c38ebc68022a94fc986391ca28
Gianluca Bàrmina <70454516+N-essuno@users.noreply.github.com>
2022-12-24 17:13:42 +0100

    when an order is sent to kitchen is marked as sent to not be taken again from the scheduled checkOrdersToSend method

--- order_management/src/main/java/it/unito/edu/scavolini/order_management/controller/RabbitMqSender.java
+++ order_management/src/main/java/it/unito/edu/scavolini/order_management/controller/RabbitMqSender.java
@@ -2,7 +2,11 @@ package it.unito.edu.scavolini.order_management.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import it.unito.edu.scavolini.order_management.enums.OrderStateEnum;
+import it.unito.edu.scavolini.order_management.model.Order;
 import it.unito.edu.scavolini.order_management.model.Preparation;
+import it.unito.edu.scavolini.order_management.repository.OrderRepository;
+import it.unito.edu.scavolini.order_management.repository.PreparationRepository;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +26,9 @@ public class RabbitMqSender {
     @Autowired
     private final Queue deliveredPreparationsQueue;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     public RabbitMqSender(RabbitTemplate rabbitTemplate, Queue kitchenQueue, Queue deliveredPreparationsQueue) {
         this.rabbitTemplate = rabbitTemplate;
         this.kitchenQueue = kitchenQueue;
@@ -37,7 +44,13 @@ public class RabbitMqSender {
         try {
             jsonPreparation = objectMapper.writeValueAsString(preparation);
             this.rabbitTemplate.convertAndSend(this.kitchenQueue.getName(), jsonPreparation);
-            System.out.println(" [x] Sent '" + preparation + "'");
+
+            // Order sent to kitchen, update order state to not take it again and resend preparations
+            Order order = preparation.getOrder();
+            order.setOrderState(OrderStateEnum.SENT_TO_KITCHEN);
+            orderRepository.save(order);
+
+            System.out.println("\n\n[S] Sent '" + preparation + "'");
         } catch (Exception e) {
             e.printStackTrace();
         }
